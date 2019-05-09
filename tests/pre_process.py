fd35f1646331dca1d9354cbd73c0f42faed28948
fthelen <46471530+fthelen@users.noreply.github.com>
2019-05-08 23:35:42 -0700

    read file

--- tests/pre_process.py
+++ tests/pre_process.py
@@ -3,14 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 
-
-
-
-df = pd.read_csv('VFSTX', parse_dates=True, index_col=0)
+df = pd.read_csv(r'tests\SP500.csv', parse_dates=True, index_col=0)
 print(df['Adj Close'].head())
-print(df.describe())
+print(df['Adj Close'].tail())
+
 print(df.shape)
 
-style.use('ggplot')
-df['Adj Close'].plot()
-plt.show()
+df['log_ret'] = np.log(df['Adj Close']/df['Adj Close'].shift(1))
+print(df['log_ret'].head())
+
+# style.use('ggplot')
+# df['Adj Close'].plot()
+# plt.show()
