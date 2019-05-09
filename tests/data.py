604e1c136110cf9a76f4cbe952384974177215b3
fthelen <46471530+fthelen@users.noreply.github.com>
2019-05-08 23:35:18 -0700

    Altered filepath

--- tests/data.py
+++ tests/data.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import fix_yahoo_finance as yf
 
-period = 'max'
-interval = '1d'
-ticker = 'VFSTX'
+period = '10y'
+interval = '1mo'
+ticker = '^GSPC'
 
 df = yf.download(ticker, period=period, interval=interval)
 
 print(df.head())
 print(df.tail())
 
-df.to_csv('VFSTX')
+df.to_csv(r'tests\SP500.csv')
