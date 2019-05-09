08838862a113b8553afd35b4ee625efeb8d230ee
fthelen <46471530+fthelen@users.noreply.github.com>
2019-05-08 22:57:20 -0700

    Made a graph

--- tests/pre_process.py
+++ tests/pre_process.py
@@ -1,9 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import style
 
-df = pd.read_csv('VFSTX', index_col='Date')
+
+
+
+df = pd.read_csv('VFSTX', parse_dates=True, index_col=0)
 print(df['Adj Close'].head())
+print(df.describe())
+print(df.shape)
 
-plt.plot(df['Adj Close'])
+style.use('ggplot')
+df['Adj Close'].plot()
 plt.show()
